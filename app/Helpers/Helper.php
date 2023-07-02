b2bd048e358f415866db8a3ee65e3edf2243f829
Sagar <sagar.dash290@gmail.com>
2023-07-02 12:13:38 +0600

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use Illuminate\Support\Str;
+
 if(!function_exists('underscore_to_camel_case')) {
     function underscore_to_camel_case($string, $capitalizeFirstCharacter = false): string
     {
@@ -15,4 +17,27 @@ if(!function_exists('underscore_to_camel_case')) {
     }
 }
 
+if(!function_exists('separate_bucket_and_path_from_string')) {
+    function separate_bucket_and_path_from_string(string $path): array
+    {
+        $pathExplode = explode('/', $path);
+        // get bucket name
+        $bucketName = $pathExplode[0];
 
+        if (count($pathExplode) > 1) {
+            $path = Str::replace($bucketName . '/', '', $path) . '/';
+        } else {
+            $path = '';
+        }
+
+        return [$bucketName, $path];
+    }
+}
+
+if ( ! function_exists('normalize_path')) {
+    function normalize_path($path): string
+    {
+        $path = Str::replace("#\/+#", "/", $path);
+        return Str::startsWith($path, '/') ? Str::replaceFirst('/', '', $path) : $path;
+    }
+}
